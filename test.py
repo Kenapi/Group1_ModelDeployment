46e95bb66133ddfe11289c5ba8b5f985315c4409
Kenapi <120555000+Kenapi@users.noreply.github.com>
2024-05-17 13:13:16 +0800

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-try:
-    model = tf.keras.models.load_model("app.h5")
-    print("Model loaded successfully.")
-except Exception as e:
-    print(f"Error loading model: {e}")
